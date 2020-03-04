30e3c13114faf2ed1bb27bdbd45de516ec0368ff
2509934810 <2509934810@qq.com>
2020-03-04 17:43:51 +0800

    add front pae

--- instance/config.py
+++ instance/config.py
@@ -1,18 +1,27 @@
 import os
 
+
 class BaseConfig(object):
     DEBUG = True
     DATA_PER_PAGE = 6
 
+
 class DevConfig(BaseConfig):
-    SECRET_KEY = '123456789'
-    DIALECT = 'mysql'
-    DRIVER = 'pymysql'
-    USERNAME = os.environ.get('USERNAME')
-    PASSWORD = os.environ.get('PASSWORD')
-    HOST = os.environ.get('HOST')
-    PORT = os.environ.get('PORT')
-    DATABASE = 'lovehome'
+    SECRET_KEY = "123456789"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.abspath(os.path.curdir)}/tmp/test.db"
+    PERMANEXT_SESSION_LIFETIME = 3
+
+
+class ProConfig(BaseConfig):
+    SECRET_KEY = "123456789"
+    DIALECT = "mysql"
+    DRIVER = "pymysql"
+    USERNAME = os.environ.get("USERNAME")
+    PASSWORD = os.environ.get("PASSWORD")
+    HOST = os.environ.get("HOST")
+    PORT = os.environ.get("PORT")
+    DATABASE = "lovehome"
     SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8mb4".format(
         DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE
     )
@@ -21,5 +30,4 @@ class DevConfig(BaseConfig):
     SQLALCHEMY_RECORD_QUERIES = True
     ARTICLES_PER_PAGE = 10
     COMMENTS_PER_PAGE = 6
-    WTF_CSRF_SECRET_KEY = 'random key for form' 
-
+    WTF_CSRF_SECRET_KEY = "random key for form"
